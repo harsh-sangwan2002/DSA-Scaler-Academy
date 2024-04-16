7575d73a414ce2c90dfd9a33b596b2cc2f5d653d
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-16 09:35:15 +0530

    added

--- Advanced/DSA 1/Arrays/Arrays 1D/rain_water.java
+++ Advanced/DSA 1/Arrays/Arrays 1D/rain_water.java
@@ -1,40 +1,40 @@
-public class rain_water {
-
+public class rain_water_trapped {
+    
     // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
     public int trap(final int[] A) {
 
-        int[] pre = new int[A.length];
-        int[] sf = new int[A.length];
+        int[] left = new int[A.length];
+        int[] right = new int[A.length];
 
-        for (int i = 0; i < A.length; i++) {
+        for(int i=0; i<A.length; i++){
 
-            if (i > 0)
-                pre[i] = Math.max(pre[i - 1], A[i]);
+            if(i==0)
+            left[i] = A[i];
 
-            else
-                pre[i] = A[i];
+            else 
+            left[i] = Math.max(left[i-1],A[i]);
         }
 
-        for (int i = A.length - 1; i >= 0; i--) {
+        for(int i=A.length-1; i>=0; i--){
 
-            if (i < A.length - 1)
-                sf[i] = Math.max(sf[i + 1], A[i]);
+            if(i==A.length-1)
+            right[i] = A[i];
 
-            else
-                sf[i] = A[i];
+            else 
+            right[i] = Math.max(right[i+1],A[i]);
         }
 
-        int ans = 0;
+        int water = 0;
 
-        for (int i = 1; i < A.length - 1; i++) {
+        for(int i=1; i<A.length-1; i++){
 
-            int left = pre[i - 1], right = sf[i + 1];
-            int water = Math.min(left, right) - A[i];
+            int block = Math.min(left[i],right[i]);
+            int curr = block - A[i];
 
-            if (water > 0)
-                ans += water;
+            if(curr>0)
+            water += curr;
         }
 
-        return ans;
+        return water;
     }
 }
