5ab012e926713e3f5fe5f28c0712a1916e949132
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-16 16:56:27 +0530

    added

--- Advanced/DSA 1/Maths/Combinatorics Basics/nCr.java
+++ Advanced/DSA 1/Maths/Combinatorics Basics/nCr.java
@@ -1,18 +1,18 @@
 public class nCr {
-
+    
     public int solve(int A, int B, int C) {
 
-        int[][] dp = new int[A + 1][B + 1];
+        int[][] dp = new int[A+1][B+1];
 
-        for (int i = 0; i < dp.length; i++) {
+        for(int i=0; i<dp.length; i++){
 
-            for (int j = 0; j <= B; j++) {
+            for(int j=0; j<=B; j++){
 
-                if (i == 0 || j == 0 || j == i)
-                    dp[i][j] = 1;
+                if(i==0 || i==1 || j==0 || j==i)
+                dp[i][j] = 1;
 
-                else
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % C;
+                else 
+                dp[i][j] = (dp[i-1][j-1]+dp[i-1][j])%C;
             }
         }
 
