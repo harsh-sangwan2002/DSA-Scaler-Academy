759193b1e39bc7feacde78f0ead4fa342cb6a3a4
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-19 15:07:38 +0530

    added

--- Advanced/DSA 2/Sorting/Count Sort & Merge Sort/merge_two_arrays.java
+++ Advanced/DSA 2/Sorting/Count Sort & Merge Sort/merge_two_arrays.java
@@ -1,30 +1,30 @@
 public class merge_two_arrays {
-    
+
     // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
     public int[] solve(final int[] A, final int[] B) {
 
         int n = A.length, m = B.length, i = 0, j = 0, k = 0;
-        int[] res = new int[n+m];
+        int[] res = new int[n + m];
 
-        while(i<n && j<m){
+        while (i < n && j < m) {
 
-            if(A[i]<B[j]){
+            if (A[i] < B[j]) {
                 res[k++] = A[i];
                 i++;
             }
 
-            else{
+            else {
                 res[k++] = B[j];
                 j++;
             }
         }
 
-        while(i<n){
+        while (i < n) {
             res[k++] = A[i];
             i++;
         }
 
-        while(j<m){
+        while (j < m) {
             res[k++] = B[j];
             j++;
         }
