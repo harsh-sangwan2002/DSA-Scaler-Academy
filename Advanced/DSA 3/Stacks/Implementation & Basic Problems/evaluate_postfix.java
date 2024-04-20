c91a472ea2d8b133ad7dcd497abfa01acb8ee097
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-20 18:19:21 +0530

    added

--- Advanced/DSA 3/Stacks/Implementation & Basic Problems/evaluate_postfix.java
+++ Advanced/DSA 3/Stacks/Implementation & Basic Problems/evaluate_postfix.java
@@ -2,40 +2,40 @@ import java.util.Stack;
 
 public class evaluate_postfix {
 
+    private int operation(int v1, int v2, String str) {
+
+        if (str.equals("+"))
+            return v1 + v2;
+
+        else if (str.equals("-"))
+            return v1 - v2;
+
+        else if (str.equals("*"))
+            return v1 * v2;
+
+        else
+            return v1 / v2;
+    }
+
     public int evalRPN(String[] A) {
 
-        Stack<String> st = new Stack<>();
+        Stack<Integer> st = new Stack<>();
 
-        for (String str : A) {
+        for (int i = 0; i < A.length; i++) {
+
+            String str = A[i];
 
             if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")) {
 
-                int val1 = Integer.parseInt(st.pop());
-                int val2 = Integer.parseInt(st.pop());
-                int val = operation(val1, val2, str);
-                st.push(val + "");
+                int v2 = st.pop();
+                int v1 = st.pop();
+                st.push(operation(v1, v2, str));
             }
 
             else
-                st.push(str);
+                st.push(Integer.parseInt(str));
         }
 
-        int res = Integer.parseInt(st.pop());
-        return res;
-    }
-
-    private int operation(int val1, int val2, String str) {
-
-        if (str.equals("+"))
-            return val1 + val2;
-
-        else if (str.equals("*"))
-            return val1 * val2;
-
-        else if (str.equals("-"))
-            return val2 - val1;
-
-        else
-            return val2 / val1;
+        return st.pop();
     }
 }
