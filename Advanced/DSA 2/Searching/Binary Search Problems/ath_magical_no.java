eddc8ed3161f613445edfea517fb6281e95a570f
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-20 14:05:10 +0530

    added

--- Advanced/DSA 2/Searching/Binary Search Problems/ath_magical_no.java
+++ Advanced/DSA 2/Searching/Binary Search Problems/ath_magical_no.java
@@ -1,43 +1,42 @@
 public class ath_magical_no {
 
-    private int GCD(int a, int b) {
+    static long mod = (long) (1e9 + 7);
+
+    private long GCD(int a, int b) {
 
         if (a == 0)
-            return b;
+            return 1L * b;
         if (b == 0)
-            return a;
+            return 1L * a;
 
         return GCD(b, a % b);
     }
 
     private long LCM(int a, int b) {
 
-        long res = a * b * 1L;
-        res = res / GCD(a, b);
-        return res;
+        long gcd = GCD(a, b);
+        long lcm = (a * b) / gcd;
+
+        return lcm;
     }
 
     public int solve(int A, int B, int C) {
 
+        long lo = 1L * Math.min(B, C), hi = lo * A, ans = 1L;
         long lcm = LCM(B, C);
-        long mod = (long) (1e9 + 7);
-        long l = Math.min(B, C) * 1L, r = l * A, ans = l;
 
-        while (l <= r) {
+        while (lo <= hi) {
 
-            long mid = l + (r - l) / 2;
-            long count = (mid / B + mid / C - mid / lcm);
+            long mid = lo + (hi - lo) / 2;
+            long count = mid / B + mid / C - mid / lcm;
 
-            if ((int) count == A) {
+            if (count < A)
+                lo = mid + 1;
+
+            else {
                 ans = mid;
-                r = mid - 1;
+                hi = mid - 1;
             }
-
-            else if ((int) count < A)
-                l = mid + 1;
-
-            else
-                r = mid - 1;
         }
 
         return (int) (ans % mod);
