ffb7ea35a6f5b509a405639683aa690ed720f45a
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-20 10:00:39 +0530

    added

---
+++ Advanced/DSA 3/Trees/LCA & Morris Traversal/recover_bst.java
@@ -0,0 +1,50 @@
+public class recover_bst {
+
+    // Definition for binary tree
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+            left = null;
+            right = null;
+        }
+    }
+
+    TreeNode p1, p2, prev;
+
+    private void inorder(TreeNode root) {
+
+        if (root == null)
+            return;
+
+        inorder(root.left);
+
+        if (prev != null && prev.val > root.val) {
+
+            if (p1 == null) {
+                p1 = prev;
+                p2 = root;
+            }
+
+            else
+                p2 = root;
+        }
+
+        prev = root;
+
+        inorder(root.right);
+    }
+
+    public int[] recoverTree(TreeNode A) {
+
+        p1 = null;
+        p2 = null;
+        prev = null;
+        inorder(A);
+
+        return new int[] { p2.val, p1.val };
+    }
+}
