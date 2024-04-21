87e67c2446b10ed23b1095a00895d8bec2307956
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-21 12:56:35 +0530

    added

--- Advanced/DSA 3/Linked List/Doubly Linked List/Deep Copy/sol.java
+++ Advanced/DSA 3/Linked List/Doubly Linked List/Deep Copy/sol.java
@@ -1,3 +1,46 @@
+import java.util.HashMap;
+
 public class sol {
-    
+
+    // Definition for singly-linked list with a random pointer.
+    class RandomListNode {
+        int label;
+        RandomListNode next, random;
+
+        RandomListNode(int x) {
+            this.label = x;
+        }
+    };
+
+    public RandomListNode copyRandomList(RandomListNode A) {
+
+        if (A == null)
+            return null;
+
+        RandomListNode head = new RandomListNode(A.label);
+        RandomListNode curr = A.next, temp = head;
+
+        HashMap<RandomListNode, RandomListNode> map = new HashMap<>();
+        map.put(A, head);
+
+        while (curr != null) {
+
+            RandomListNode node = new RandomListNode(curr.label);
+            temp.next = node;
+            temp = temp.next;
+
+            map.put(curr, node);
+            curr = curr.next;
+        }
+
+        curr = A;
+        while (curr != null) {
+
+            RandomListNode random = curr.random;
+            map.get(curr).random = map.get(random);
+            curr = curr.next;
+        }
+
+        return head;
+    }
 }
