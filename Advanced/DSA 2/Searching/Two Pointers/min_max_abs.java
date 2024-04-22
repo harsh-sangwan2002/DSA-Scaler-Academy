fc380331e4c0e7ee3c8472a421c476db1e7bbf91
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-22 19:22:26 +0530

    added

--- Advanced/DSA 2/Searching/Two Pointers/min_max_abs.java
+++ Advanced/DSA 2/Searching/Two Pointers/min_max_abs.java
@@ -2,25 +2,32 @@ public class min_max_abs {
 
     // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
     public int minimize(final int[] A, final int[] B, final int[] C) {
-        int diff = Integer.MAX_VALUE;
-        int minimum = Integer.MAX_VALUE;
-        int maximum = Integer.MIN_VALUE;
-        int i, j, k;
-        for (i = 0, j = 0, k = 0; i < A.length && j < B.length && k < C.length;) {
-            // max(abs(A[i] - B[j]), abs(B[j] - C[k]), abs(C[k] - A[i])) = max(A[i], B[j],
-            // C[k]) - min(A[i], B[j], C[k])
-            minimum = Math.min(A[i], Math.min(B[j], C[k]));
-            maximum = Math.max(A[i], Math.max(B[j], C[k]));
-            diff = Math.min(diff, maximum - minimum);
+
+        int i = 0, j = 0, k = 0;
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
+
+        while (i < A.length && j < B.length && k < C.length) {
+
+            max = Math.max(A[i], Math.max(B[j], C[k]));
+            min = Math.min(A[i], Math.min(B[j], C[k]));
+
+            int diff = max - min;
+            ans = Math.min(ans, diff);
+
             if (diff == 0)
                 break;
-            if (A[i] == minimum)
+
+            if (A[i] == min)
                 i++;
-            else if (B[j] == minimum)
+
+            else if (B[j] == min)
                 j++;
+
             else
                 k++;
         }
-        return diff;
+
+        return ans;
     }
 }
