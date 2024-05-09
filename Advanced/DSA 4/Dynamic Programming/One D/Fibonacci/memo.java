9af2fa3c993509f31a7fef295d2700d9fba5ff81
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-09 13:37:46 +0530

    added

---
+++ Advanced/DSA 4/Dynamic Programming/One D/Fibonacci/memo.java
@@ -0,0 +1,34 @@
+package Fibonacci;
+
+import java.util.*;
+
+public class memo {
+
+    private static int solve(int n, int[] dp) {
+
+        if (n == 0 || n == 1)
+            return n;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = solve(n - 1, dp) + solve(n - 2, dp);
+    }
+
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 0; i < dp.length; i++)
+            dp[i] = -1;
+
+        int res = solve(n, dp);
+        System.out.print(res);
+    }
+}
---
+++ Advanced/DSA 4/Dynamic Programming/One D/Fibonacci/optimised.java
@@ -0,0 +1,26 @@
+package Fibonacci;
+
+import java.util.*;
+
+public class optimised {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int a = 0, b = 1, c = a + b;
+
+        for (int i = 2; i <= n; i++) {
+
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        System.out.print(c);
+        scn.close();
+    }
+}
