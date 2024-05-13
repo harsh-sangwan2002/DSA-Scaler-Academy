230583b767ef848cbfff27d07348c4b64447725f
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-05-13 15:47:21 +0530

    added

---
+++ Advanced/DSA 1/Arrays/Arrays 2D/min_swaps.java
@@ -0,0 +1,39 @@
+public class min_swaps {
+
+    public int solve(int[] A, int B) {
+
+        int count = 0;
+
+        for (int val : A) {
+
+            if (val <= B)
+                count++;
+        }
+
+        if (count == A.length)
+            return 0;
+
+        int bad = 0, swaps = 0;
+
+        for (int i = 0; i < count; i++) {
+
+            if (A[i] > B)
+                bad++;
+        }
+
+        swaps = bad;
+        for (int i = count; i < A.length; i++) {
+
+            int prev = A[i - count];
+            if (prev > B)
+                bad--;
+
+            if (A[i] > B)
+                bad++;
+
+            swaps = Math.min(swaps, bad);
+        }
+
+        return swaps;
+    }
+}
