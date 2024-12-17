e6bbd460cca545b856fd34f7850ba6b65357d15f
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-12-17 08:24:35 +0530

    added swap.java

---
+++ Intermediate/DSA 2/Memory Management/swap.java
@@ -0,0 +1,25 @@
+public class swap {
+
+    public static void swap(int[] temp1, int[] temp2) {
+
+        int[] temp = temp1;
+        temp1 = temp2;
+        temp2 = temp;
+    }
+
+    public static void main(String... args) {
+
+        int[] arr1 = { 1, 2, 3, 4, 5 };
+        int[] arr2 = { 6, 7, 8, 9, 10 };
+
+        swap(arr1, arr2);
+
+        for (int val : arr1)
+            System.out.println(val);
+
+        System.out.println("------------------");
+
+        for (int val : arr2)
+            System.out.println(val);
+    }
+}
